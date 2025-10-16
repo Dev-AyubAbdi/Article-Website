8022d94aa0afb8df632e166aea52df1ce4fa510c
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2025-10-16 04:40:16 +0300

    added

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,3 +1,41 @@
+import { Link } from "react-router";
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="bg-white">
+        <div className="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
+          {/* nav Links */}
+          <nav className="-mx-5 -m7-2 flex flex-wrap justify-center">
+            <div className="px-5 py-2">
+              <Link to="/" className="text-base text-gray-500 hover:text-gray-900">Home</Link>
+               
+            </div>
+            <div className="px-5 py-2">
+              <Link to="/articles" className="text-base text-gray-500 hover:text-gray-900">Articles</Link>
+               
+            </div>
+            <div className="px-5 py-2">
+              <Link to="/articles" className="text-base text-gray-500 hover:text-gray-900">Articles</Link>
+               
+            </div>
+            <div className="px-5 py-2">
+             <Link to="/tags" className="text-base text-gray-500 hover:text-gray-900">Tags</Link>
+               
+            </div>
+             <div className="px-5 py-2">
+               <Link to="/aboutus" className="text-base text-gray-500 hover:text-gray-900">About Us</Link>
+               
+            </div>
+             <div className="px-5 py-2">
+               <Link to="/contect" className="text-base text-gray-500 hover:text-gray-900">Contect</Link>
+               
+            </div>
+           
+               
+               
+                 
+          </nav>
+          {/* social media links */}
+        </div>
+    </div>
+  )
 };
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -129,61 +129,82 @@ export const Header = () => {
                 onClick={() => setIsMenuOpen(!isMenuOpen)}
                 className="inline-flex items-center justify-center p-2 rounded-md text-gray-400"
               >
-                {isMenuOpen ? <CiMenuBurger className="block w-6 h-6" /> : <IoMdClose className="block w-6 h-6" />}
+                {isMenuOpen ? (
+                  <CiMenuBurger className="block w-6 h-6" />
+                ) : (
+                  <IoMdClose className="block w-6 h-6" />
+                )}
               </button>
             </div>
           </div>
         </div>
       </div>
       {/* menu menu */}
-      {
-        isMenuOpen && (
-      <div className="sm:hidden">
-        <div className="pt-2 pb-3 space-y-1">
-             <Link
-                to="/"
-                className="block pl-3 pr-4 py-2  items-center px-1 pt-1 border-l-2 border-orange-500 text-base font-medium text-gray-900 text-orange-700 bg-orange-50"
-              >
-                Home
-              </Link>
-              <Link
-                to="/articles"
-                className=" block pl-3 pr-4 py-2 px-1 pt-1 border-b-2 border-transparent text-sm font-medium text-gray-600 focus:bg-gray-50 hover:border-gray-300 hover:text-gray-800"
-              >
-                Articles
-              </Link>
-        </div>
-      </div>
-        )
-    }
-    {
+      {isMenuOpen && (
+        <div className="sm:hidden py-4">
+          <div className="pt-2 pb-3 space-y-1">
+            <Link
+              to="/"
+              className="block pl-3 pr-4 py-2  items-center px-1 pt-1 border-l-2 border-orange-500 text-base font-medium text-gray-900 text-orange-700 bg-orange-50"
+            >
+              Home
+            </Link>
+            <Link
+              to="/articles"
+              className=" block pl-3 pr-4 py-2 px-1 pt-1 border-b-2 border-transparent text-base font-medium text-gray-600 focus:bg-gray-50 hover:border-gray-300 hover:text-gray-800"
+            >
+              Articles
+            </Link>
+          </div>
+          {/* Is Logged in */}
+           {
         isLoggedIn && (
              <div>
              <Link
                 to="/articles"
-                className=" block pl-3 pr-4 py-2 px-1 pt-1 border-b-2 border-transparent text-sm font-medium text-gray-600 focus:bg-gray-50 hover:border-gray-300 hover:text-gray-800"
+                className=" block pl-3 pr-4 py-2 px-1 pt-1 border-b-2 border-transparent text-base font-medium text-gray-600 focus:bg-gray-50 hover:border-gray-300 hover:text-gray-800"
               >
                 Write
               </Link>
                <Link
                 to="/articles"
-                className=" block pl-3 pr-4 py-2 px-1 pt-1 border-b-2 border-transparent text-sm font-medium text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800"
+                className=" block pl-3 pr-4 py-2 px-1 pt-1 border-b-2 border-transparent text-base font-medium text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800"
               >
                My Articles
               </Link>
                <Link
                 to="/articles"
-                className=" block pl-3 pr-4 py-2 px-1 pt-1 border-b-2 border-transparent text-sm font-medium text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800"
+                className=" block pl-3 pr-4 py-2 px-1 pt-1 border-b-2 border-transparent text-base font-medium text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800"
               >
                 My profile
               </Link>
-              <button className="block w-full text-left pl-3 pr-4 py-2 border-l-4 border-transparent text-sm font-medium text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800">
+              <button className="block w-full text-left pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800">
                 Sign out
               </button>
           </div> 
         )
     }
-    
+    {/* if is not  logged in */}
+    {
+      !isLoggedIn && (
+        <>
+        <Link
+                to="/articles"
+                className=" block pl-3 pr-4 py-2 px-1 pt-1 border-l-4 border-transparent text-base font-medium text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800"
+              >
+                Sign In
+              </Link>
+              <Link
+                to="/articles"
+                className=" block pl-3 pr-4 py-2 px-1 pt-1 border-l-4 border-transparent text-base font-medium text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800"
+              >
+                Sing Up
+              </Link>
+        </>
+      )
+    }
+        </div>
+      )}
     </header>
   );
 };
